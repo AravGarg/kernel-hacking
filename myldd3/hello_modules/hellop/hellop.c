24d80a50f01614ce86269ad6f90ea47e7c410719
AravGarg <arav1635@gmail.com>
2020-10-26 16:26:56 +0400

    Added driver for hello-world with arguments

--- myldd3/hello_modules/hellop/hellop.c
+++ myldd3/hello_modules/hellop/hellop.c
@@ -1,25 +1,24 @@
-#include<linux/module.h>
 #include<linux/init.h>
+#include<linux/module.h>
 #include<linux/moduleparam.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
-
-static int howmany=1;
-static int i=0;
 static char *whom="World";
+static int howmany=1;
 module_param(howmany,int,S_IRUGO);
 module_param(whom,charp,S_IRUGO);
+int i;
 
-static int __init my_init(void){
+static int __init hello_init(void){
 	for(i=0;i<howmany;i++){
-		printk(KERN_DEBUG "Hello,%s\n",whom);
+		printk(KERN_ALERT "Hello %s.\n",whom);
 	}
 	return 0;
 }
 
-static void __exit my_exit(void){
-	printk(KERN_DEBUG "Goodbye.\n");
+static void __exit hello_exit(void){
+	printk(KERN_ALERT "Goodbye.\n");
 }
-module_init(my_init);
-module_exit(my_exit);
 
+module_init(hello_init);
+module_exit(hello_exit);
